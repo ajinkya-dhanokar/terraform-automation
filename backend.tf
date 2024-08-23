6e123a616713d4b7afc0b4964e8038954434cd58
ajinkya-dhanokar <132273478+ajinkya-dhanokar@users.noreply.github.com>
2024-08-23 22:08:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aj-terraform-project-1"
-    key = "b18-verginia"
+    key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
   }
