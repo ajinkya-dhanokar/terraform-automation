2731cf11d3e3da971f8dfe47566d2698ba75f5cd
ajinkya-dhanokar <132273478+ajinkya-dhanokar@users.noreply.github.com>
2024-08-23 22:32:32 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "b18-verginia" 
+    default     =  "jenkins-server-key" 
 }
 
 variable "instance_type" { 
